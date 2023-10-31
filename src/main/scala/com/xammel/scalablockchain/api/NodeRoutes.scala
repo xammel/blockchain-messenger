f6d1763fed6d267838f8b62e575dc4b84c085680
Max Melhuish <maxmelhuish@gmail.com>
2023-10-31 22:58:23 +1100

    started writing the API

---
+++ src/main/scala/com/xammel/scalablockchain/api/NodeRoutes.scala
@@ -0,0 +1,5 @@
+package com.xammel.scalablockchain.api
+
+trait NodeRoutes {
+
+}
---
+++ src/main/scala/com/xammel/scalablockchain/api/Server.scala
@@ -0,0 +1,34 @@
+package com.xammel.scalablockchain.api
+
+import akka.actor.{ActorRef, ActorSystem}
+import akka.cluster.pubsub.DistributedPubSub
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import com.typesafe.config.{Config, ConfigFactory}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import com.xammel.scalablockchain.actors.Node
+object Server extends App with NodeRoutes {
+
+  //TODO review this args parsing
+  private val address = if (args.length > 0) args(0) else "localhost"
+  private val port = if (args.length > 1) args(1).toInt else 8080
+
+  implicit val system: ActorSystem = ActorSystem("scala-blockchain")
+
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  val node: ActorRef = system.actorOf(Node.props("scalaBlockchainNode0"))
+
+  private lazy val routes: Route = statusRoutes ~ transactionRoutes ~ mineRoutes
+
+  Http().bindAndHandle(routes, address, port)
+
+  println(s"Server online at http://$address:$port/")
+
+  Await.result(system.whenTerminated, Duration.Inf)
+
+}
