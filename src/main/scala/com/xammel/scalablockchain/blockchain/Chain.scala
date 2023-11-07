507616145b21495cde906cee0f0d8313cee77c75
Max Melhuish <maxmelhuish@gmail.com>
2023-11-08 00:04:45 +1100

    cleanup and small improvements

--- src/main/scala/com/xammel/scalablockchain/blockchain/Chain.scala
+++ src/main/scala/com/xammel/scalablockchain/blockchain/Chain.scala
@@ -4,58 +4,13 @@ import com.xammel.scalablockchain.crypto.Crypto
 import com.xammel.scalablockchain.crypto.Crypto.sha256Hash
 import com.xammel.scalablockchain.json.JsonSupport.PopulatedBlockFormat
 import spray.json._
-
-//TODO, should Chain be Block, and then also have another Blockchain data structure that takes a Seq[Block] ?
-
-//sealed trait Chain {
-//  //TODO I think this is actually latestIndex in the chain?
-//  val index: Int
-//  //TODO I think this is similarly latestHash in chain
-//  val previousHash: String
-//  val transactions: List[Transaction]
-//  val proof: Long
-//  val timestamp: Long
-//
-//  //TODO pretty sure this doesn't work as intended, think tail should concatenate rather than overwrite
-//  def ::(link: Chain): Chain = link match {
-//    case link: ChainLink => link.copy(previousHash = this.previousHash, tail = this)
-//    case EmptyChain      => throw new InvalidParameterException(emptyChainAdditionMessage)
-//  }
-//}
-//
-//object Chain {
-//  def apply(chainLinks: Seq[Chain]): Chain = chainLinks match {
-//    case Nil => EmptyChain
-//    case (link: ChainLink) :: tail =>
-//      ChainLink(link.index, link.proof, link.transactions, link.previousHash, apply(tail))
-//  }
-//}
-//
-//case class ChainLink(
-//    index: Int,
-//    proof: Long,
-//    transactions: List[Transaction],
-//    previousHash: String = "",
-//    tail: Chain = EmptyChain,
-//    timestamp: Long = System.currentTimeMillis()
-//) extends Chain {
-//  val hash = sha256Hash(this.toJson.toString)
-//}
-//
-////TODO review these defaults
-//case object EmptyChain extends Chain {
-//  override val index: Int                      = 0
-//  override val previousHash: String            = "1"
-//  override val transactions: List[Transaction] = List.empty
-//  override val proof: Long                     = 100
-//  override val timestamp: Long                 = System.currentTimeMillis()
-//}
+import scala.util.Random
 
 sealed trait Chain {
   val blocks: List[Block]
   def numberOfBlocks               = blocks.length
-  def mostRecentBlocksIndex: Long  = blocks.minBy(_.timestamp).index //TODO make safe accessor?
-  def mostRecentBlocksHash: String = blocks.minBy(_.timestamp).hash  //TODO make safe accessor?
+  def mostRecentBlocksIndex: Long  = blocks.minBy(_.timestamp).index //TODO unsafe accessor
+  def mostRecentBlocksHash: String = blocks.minBy(_.timestamp).hash  //TODO unsafe accessor
 
   def addBlock(transactions: List[Transaction], proof: Long): NonEmptyChain =
     NonEmptyChain(blocks = this.blocks :+ createNextBlock(transactions, proof))
@@ -68,8 +23,6 @@ sealed trait Chain {
       timestamp = System.currentTimeMillis()
     )
 }
-
-object Chain {}
 
 case class NonEmptyChain(blocks: List[Block]) extends Chain
 
@@ -93,7 +46,8 @@ case class PopulatedBlock(
 }
 
 case object GenesisBlock extends Block {
+  private val randomSeedString = Random.alphanumeric.take(10).toString
   override val index: Long     = 0
-  override val hash: String    = Crypto.sha256Hash("GenesisBlock")
+  override val hash: String    = Crypto.sha256Hash(randomSeedString)
   override val timestamp: Long = System.currentTimeMillis()
 }
--- src/main/scala/com/xammel/scalablockchain/crypto/Crypto.scala
+++ src/main/scala/com/xammel/scalablockchain/crypto/Crypto.scala
@@ -6,6 +6,10 @@ import java.security.MessageDigest
 object Crypto {
 
   //TODO review this implementation
+  //val encoder: MessageDigest = MessageDigest.getInstance(sha256)
+  //    val codeVerifierBytes: Array[Byte] = codeVerifier.getBytes(StandardCharsets.UTF_8)
+  //    val data: Array[Byte] = encoder.digest(codeVerifierBytes)
+  //?
   def sha256Hash(value: String) = String.format("%064x",
     new BigInteger(1, MessageDigest.getInstance("SHA-256").digest(value.getBytes("UTF-8"))))
 
